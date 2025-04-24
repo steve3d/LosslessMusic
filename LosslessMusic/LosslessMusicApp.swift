a2ad4b177cf3c5b36469d0e75992262d2df1c0d0
印江 <steve@chinavfx.net>
2025-04-24 09:57:49 +0800

    Initial Commit

---
+++ LosslessMusic/LosslessMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LosslessMusicApp.swift
+//  LosslessMusic
+//
+//  Created by 印江 on 2025/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LosslessMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LosslessMusicTests/LosslessMusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  LosslessMusicTests.swift
+//  LosslessMusicTests
+//
+//  Created by 印江 on 2025/4/24.
+//
+
+import Testing
+@testable import LosslessMusic
+
+struct LosslessMusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
