8c1ba0963fce4570cdbb6e03b22fbd8e4e5d53f7
印江 <steve@chinavfx.net>
2025-04-25 14:44:06 +0800

    reorganize code

--- LosslessMusic/LogMonitor.swift
+++ LosslessMusic/LogMonitor.swift
@@ -12,6 +12,14 @@ class LogMonitor {
     private var process: Process?
     private var pipe: Pipe?
     private var defaultDeviceId: AudioDeviceID = 0
+    
+    // media format info for lossless only
+    private let mediaFormatPattern = /play\> cm\>> mediaFormatinfo.*lossless,/
+    
+    private let bitDepthPattern = /sdBitDepth = (\d+)/
+    private let sampleRatePattern = /asbdSampleRate = (\d+(?:\.\d+)?)/
+    private let formatIdPattern = /asbdFormatID = (\w+)/
+    private let numChannelsPattern = /asbdNumChannels = (\d+)/
 
     
     // Callback for media start to play, use call back for extreme simple solution
@@ -44,64 +52,14 @@ class LogMonitor {
             "compact" // Use compact style for simpler parsing
         ]
         process?.standardOutput = pipe // only need standard output
-               
-        // media format info for lossless only
-        let mediaFormatPattern = /play\> cm\>> mediaFormatinfo.*lossless,/
         
-        let bitDepthPattern = /sdBitDepth = (\d+)/
-        let sampleRatePattern = /asbdSampleRate = (\d+(?:\.\d+)?)/
-        let formatIdPattern = /asbdFormatID = (\w+)/
-        let numChannelsPattern = /asbdNumChannels = (\d+)/
-        
-        
-        var bitDepth: UInt32?
-        var sampleRate: Double?
-        var numChannels: UInt32?
-        var asbdFormatId: String?
-        
-
         // Handle log output
         // FIXME: after sleep, this might be dead
         pipe?.fileHandleForReading.readabilityHandler = { [weak self] handle in
             let data = handle.availableData
             if let logMessage = String(data: data, encoding: .utf8), !logMessage.isEmpty {
                 // Process each log line
-                let lines = logMessage.split(separator: "\n")
-                for line in lines {
-                    // found lossless media format info log
-                    if line.contains(mediaFormatPattern) {
-                        bitDepth = nil
-                        sampleRate = nil
-                        numChannels = nil
-                        asbdFormatId = nil
-                        if let m = line.firstMatch(of: bitDepthPattern) {
-                            bitDepth = UInt32(m.1)
-                        }
-                        
-                        if let m = line.firstMatch(of: sampleRatePattern) {
-                            sampleRate = Double(m.1)
-                        }
-                        
-                        if let m = line.firstMatch(of: numChannelsPattern) {
-                            numChannels = UInt32(m.1)
-                        }
-                        
-                        if let m = line.firstMatch(of: formatIdPattern) {
-                            asbdFormatId = String(m.1)
-                        }
-
-                        
-                        if let bitDepth, let sampleRate, let numChannels, let asbdFormatId {
-                            print("New playing media format (\(asbdFormatId)) : nbChannels: \(numChannels), bitDepth: \(bitDepth), sampleRate: \(sampleRate * 1000)")
-                            
-                            // must run on main thread
-                            DispatchQueue.main.async {
-                                self?.newFormatDetected?(numChannels, bitDepth, sampleRate * 1000, asbdFormatId)
-                            }
-                        }
-                    }
-                    
-                }
+                logMessage.split(separator: "\n").forEach { self?.processLogLine($0) }
             }
         }
 
@@ -110,6 +68,46 @@ class LogMonitor {
             try process?.run()
         } catch {
             print("Failed to launch log stream process: \(error)")
+        }
+    }
+    
+    private func processLogLine(_ line: String.SubSequence) {
+        var bitDepth: UInt32?
+        var sampleRate: Double?
+        var numChannels: UInt32?
+        var asbdFormatId: String?
+        
+        // find lossless media format info log
+        if line.contains(mediaFormatPattern) {
+            bitDepth = nil
+            sampleRate = nil
+            numChannels = nil
+            asbdFormatId = nil
+            if let m = line.firstMatch(of: bitDepthPattern) {
+                bitDepth = UInt32(m.1)
+            }
+            
+            if let m = line.firstMatch(of: sampleRatePattern) {
+                sampleRate = Double(m.1)
+            }
+            
+            if let m = line.firstMatch(of: numChannelsPattern) {
+                numChannels = UInt32(m.1)
+            }
+            
+            if let m = line.firstMatch(of: formatIdPattern) {
+                asbdFormatId = String(m.1)
+            }
+
+            
+            if let bitDepth, let sampleRate, let numChannels, let asbdFormatId {
+                print("New playing media format (\(asbdFormatId)) : nbChannels: \(numChannels), bitDepth: \(bitDepth), sampleRate: \(sampleRate * 1000)")
+                
+                // must run on main thread
+                DispatchQueue.main.async { [self] in
+                    newFormatDetected?(numChannels, bitDepth, sampleRate * 1000, asbdFormatId)
+                }
+            }
         }
     }
 }
